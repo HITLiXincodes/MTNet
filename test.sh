efc7753925c5997e3c347ccff60642f7a806c8e2
LiXin <83623227+HITLiXincodes@users.noreply.github.com>
2025-10-25 14:12:54 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,4 +0,0 @@
-python test.py --gpus 1 --model ctcnet --name final1 \
-    --load_size 128 --dataset_name single --dataroot "/home/ubuntu/thumb_data/" \
-    --pretrain_model_path "/home/ubuntu/hog/Ablation4-encry2/checkpoints/ab16_1/iter_200000_net_G.pth" \
-    --save_as_dir "/home/ubuntu/hog/Ablation4-encry2/ff/"
